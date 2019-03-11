32749bd57164f4da1d369e0d3ecfae0de9d93d05
daniel <9087597+daankrs@users.noreply.github.com>
2019-03-11 19:17:03 +0100

    Initial commit

---
+++ Assets/Controllers/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 50f;
+    public AudioSource deathSound;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            deathSound.Play(); 
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject,0.5f);
+    }
+}
---
+++ Assets/FirstPersonCharacter/Scripts/GunScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public Camera fpsCam;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        muzzleFlash.Play();
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                Debug.Log("HITENENMY");
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject ImpactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(ImpactGO, 2f);
+        }
+
+
+    }
+}
